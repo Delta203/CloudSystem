315d16587a136c2831fe0e8c1239635daf9ef87b
daviddf <daviddf@campus-uni.paderborn.de>
2024-06-01 01:58:03 +0200

    licence headers

--- Cloud-Master/src/main/java/de/cloud/master/delta203/core/files/FileManager.java
+++ Cloud-Master/src/main/java/de/cloud/master/delta203/core/files/FileManager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Cloud System by Delta203
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.cloud.master.delta203.core.files;
 
 import com.google.gson.Gson;
@@ -9,27 +25,17 @@ import java.io.*;
 
 public class FileManager {
 
-  private final String path;
   private final String name;
   private final File file;
   private JsonObject data;
 
   public FileManager(String path, String name) {
-    this.path = path;
     this.name = name;
     file = new File(path, name);
   }
 
-  public String getPath() {
-    return path;
-  }
-
   public String getName() {
     return name;
-  }
-
-  public File getFile() {
-    return file;
   }
 
   public void load() {
