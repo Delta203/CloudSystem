8c42501677a7edfe39328bccaaedc348ddeb40c0
daviddf <daviddf@campus-uni.paderborn.de>
2024-09-05 17:35:29 +0200

    fix

--- Cloud-API/src/main/java/de/cloud/api/delta203/server/listeners/CloudListenerOnlyProxy.java
+++ Cloud-API/src/main/java/de/cloud/api/delta203/server/listeners/CloudListenerOnlyProxy.java
@@ -31,9 +31,7 @@ public class CloudListenerOnlyProxy implements Listener {
 
   @EventHandler(priority = EventPriority.HIGHEST)
   public void onLogin(PlayerLoginEvent e) {
-    if (e.getRealAddress().getHostAddress().equals(CloudInstance.ip)
-        || e.getAddress().getHostAddress().equals("localhost")
-        || e.getAddress().getHostAddress().equals("127.0.0.1")) return;
+    if (e.getRealAddress().getHostAddress().equals(CloudInstance.ip)) return;
     e.disallow(
         PlayerLoginEvent.Result.KICK_OTHER, ChatColor.RED + "You must join through the proxy.");
   }
